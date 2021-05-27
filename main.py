d1a5dd168c3936e615a12520bffeefc5a96130a7
benrprince <princebe@oregonstate.edu>
2021-05-27 18:44:07 -0500

    inital commits

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #############################################
 # Author: Ben Prince
-# Date: 5/10/2021
-# CS_493 Assignment 7
+# Date: 06/07/2021
+# CS_493 Final Project
 ############################################
 
 # Google and Flask imports
@@ -16,13 +16,27 @@ import requests
 import random
 import json
 
+# Set up Flask, datastore and secret key for session variables
 app = Flask(__name__)
 client = datastore.Client()
 app.config['SECRET_KEY'] = 'sessionsecret'
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return "Please Navigate to /welcome"
+
+@app.route('/welcome', methods=["GET", "POST"])
+def welcome():
+
+    if request.method == "GET":
+        return render_template("welcome.html")
+
+    elif request.method == "POST":
+        return
+
+    else:
+
+        return render_template("welcome.html")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
